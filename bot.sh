7a974cb9ffbea7f3eba37fa896640be058100b70
Bquce <87562853+Bquce@users.noreply.github.com>
2021-07-17 16:02:47 +0300

    Add files via upload

---
+++ bot.sh
@@ -0,0 +1,16 @@
+apt update
+apt upgrade
+apt install python3.9 -y
+apt install python3-pip -y
+apt install screen -y
+pip3 install aiogram
+pip3 install rich
+pip3 install pyyalm
+apt install python3-venv -y
+python3 -m venv xxx
+source xxx/bin/activate
+apt update
+pip install aiogram
+pip install rich
+pip install pyyalm
+screen -S xxx python -m app
